a57b540aa7316293a04d70878f131040df8dc9eb
Sayem92 <sayemmiha123@gmail.com>
2023-04-24 22:24:41 +0600

    add responsive

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <div className="">
+    <div className="mx-w-[1440px] mx-auto">
       <RouterProvider router={router}></RouterProvider>
       <Toaster></Toaster>
     </div>
