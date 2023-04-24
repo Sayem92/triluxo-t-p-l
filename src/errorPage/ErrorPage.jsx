72d213b55d90033ed14d281b88321c47b789003b
Sayem92 <sayemmiha123@gmail.com>
2023-04-24 22:35:07 +0600

    add alt

--- src/errorPage/ErrorPage.jsx
+++ src/errorPage/ErrorPage.jsx
@@ -24,12 +24,12 @@ const ErrorPage = () => {
               </div>
             </div>
             <div>
-              <img src="https://i.ibb.co/G9DC8S0/404-2.png" />
+              <img src="https://i.ibb.co/G9DC8S0/404-2.png" alt=""/>
             </div>
           </div>
         </div>
         <div>
-          <img src="https://i.ibb.co/ck1SGFJ/Group.png" />
+          <img src="https://i.ibb.co/ck1SGFJ/Group.png" alt=""/>
         </div>
       </div>
     </div>
